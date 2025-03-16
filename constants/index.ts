f93604e7bd1970ff39359a1dee9e18e82db0fb0b
howiden <m17366441685@163.com>
2025-03-16 17:44:48 +0800

    upload initial code

---
+++ constants/index.ts
@@ -0,0 +1,50 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/', key: 'game', label: 'Games' },
+  { href: '/', key: 'community', label: 'Community' },
+  { href: '/', key: 'contact_us', label: 'Contact Us' },
+];
+
+// PROMOTION SECTION
+
+// GAMES SECTION
+
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: 'Learn More',
+    links: [
+      'About Mind Mingle',
+      'Press Releases',
+      'Environment',
+      'Jobs',
+      'Privacy Policy',
+      'Contact Us',
+    ],
+  },
+  {
+    title: 'Our Community',
+    links: ['Climbing xixixi', 'Hiking hilink', 'Hilink kinthill'],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Contact Us',
+  links: [
+    { label: 'Admin Officer', value: '123-456-7890' },
+    { label: 'Email Officer', value: 'hilink@akinthil.com' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Social',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg',
+    '/youtube.svg',
+    '/wordpress.svg',
+  ],
+};
