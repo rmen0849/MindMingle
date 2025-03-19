2d906a3ec4c381bfaf2b34e19e8b9d2b4d4281ed
howiden <m17366441685@163.com>
2025-03-19 23:13:55 +0800

    improve

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-// import Camp from "@/components/Camp";
-// import Features from "@/components/Features";
-// import GetApp from "@/components/GetApp";
-// import Guide from "@/components/Guide";
-// import Hero from "@/components/Hero";
-
-
-import Login from "@/components/Login"; // 只导入 Login 组件
+import Promotion from "@/components/Promotion";
 
 export default function Home() {
-  return <Login />; // 只渲染 Login 组件，不要其他组件
+  return (
+    <>
+      <Promotion />
+      {/* <Games />
+      <Community /> */}
+    </>
+  )
 }
